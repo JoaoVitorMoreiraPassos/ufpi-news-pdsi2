2a3ed6cfd6dc09443ef10a4ecadb401d97473935
dayangomes <dayanramos5537@ufpi.edu.br>
2024-05-14 16:31:16 -0300

    feat: Criando permissão de postagem de notícias apenas para as contas autorizadas

---
+++ backend/posts/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+class HasPostPermissions(permissions.BasePermission):
+    """
+    Custom permission to only allow users with post_permissoes=True to POST, UPDATE and DELETE
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            # Para GET, OPTIONS e HEAD, qualquer usuário pode acessar
+            return True
+        else:
+            # Para POST, UPDATE e DELETE, o usuário precisa ter a permissão post_permissoes=True
+            return request.user.post_permissoes or request.user.is_superuser
